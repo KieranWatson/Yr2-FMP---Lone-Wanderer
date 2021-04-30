94de8a6cf8325d1159bd00cb57107d8e32b3d5bc
Kieran Watson <61968335+KieranWatson@users.noreply.github.com>
2021-04-30 14:36:20 +0100

    Added a scene transition

---
+++ Assets/Scripts/Menu/Menu_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Script : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame() 
+    {
+        SceneManager.LoadScene("Game_Scene");
+    }
+
+    void ExitGame() 
+    {
+        
+    }
+}
